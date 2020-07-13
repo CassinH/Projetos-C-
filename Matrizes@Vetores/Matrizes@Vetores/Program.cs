e5f22f558dab45d053ef05769ea60ac0f93416e8
CassinH <52459763+CassinH@users.noreply.github.com>
2020-07-13 13:31:57 -0300

    Programação Orientada OBJS

---
+++ Matrizes@Vetores/Matrizes@Vetores/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Matrizes_Vetores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] n = new int[3, 5];
+            /*
+             10 20 30 40 50
+             60 70 80 90 15
+             25 35 45 55 65
+             */
+
+            n[0,0] = 10;
+            n[0,1] = 20;
+            n[0,2] = 30;
+            n[0,3] = 40;
+            n[0, 4] = 50;
+
+            n[1, 0] = 60;
+            n[1, 1] = 70;
+            n[1, 2] = 80;
+            n[1, 3] = 90;
+            n[1, 4] = 15;
+
+            n[2, 0] = 60;
+            n[2, 1] = 70;
+            n[2, 2] = 80;
+            n[2, 3] = 90;
+            n[2, 4] = 15;
+
+            Console.WriteLine(n[1,5]);
+
+
+
+        }
+    }
+}
---
+++ Senha Errada@WHILEeDO/Senha Errada@WHILEeDO/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Senha_Errada_WHILEeDO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String senha = "1010";
+            String senhauser;
+            int tentativas = 0;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Digite a senha");
+                senhauser = Console.ReadLine();
+                tentativas++;
+            } while (senha != senhauser);
+           
+            Console.Clear();
+            Console.WriteLine("Senha Correta,tentativas:{0}", tentativas);
+        }
+    }
+}
